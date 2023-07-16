87ddff2e1c86e5f75840b9f49423d82728244ab1
Ataerk <114510382+Zank613@users.noreply.github.com>
2023-07-16 19:20:43 +0300

    Updated the task_manager file.
    Added to ability to sort. Added dates instead of using float values as deadlines.

--- task_manager.py
+++ task_manager.py
@@ -1,27 +1,28 @@
 import csv ,operator
 import os
 
-
+# Task class for adding attribute.
 class Task:
     def __init__(self, name: str, deadline: float, description: str, priority=0):
         self.name = name
         self.deadline = deadline
         self.description = description
         self.priority = priority
-
+    # Tasks did not print out as readable so i did this.
     def __repr__(self):
         return f"Name: {self.name}\nDeadline: {self.deadline}\nDescription: {self.description}\nPriority: {self.priority}\n"
 
 
-
+# Class to manage Tasks.
 class TaskManager:
     def __init__(self, file_path):
         self.tasks = []
         self.file_path = file_path
         self.load_from_csv()
 
-
+    # Method to load data from .csv file.
     def load_from_csv(self):
+        # Checks if there is a file called task_table.csv if not, this method creates one.
         if not os.path.exists(self.file_path):
             with open(self.file_path, 'w', newline='') as f:
                 writer = csv.writer(f)
@@ -69,6 +70,7 @@ class TaskManager:
                 return task
         return None
 
+    # This editing method took me hours to make.
     def edit_task(self, name):
         task = self.get_task_by_name(name)
         if task is not None:
@@ -100,12 +102,15 @@ class TaskManager:
             self.write_to_csv("task_table.csv")
             print("Task edited successfully.")
         else:
-            print("No task with the given name found.")        
+            print("No task with the given name found.")
 
+        
+# Calling TaskManager class to manager varible.
 manager = TaskManager("task_table.csv")
 
-
+# This whole loop is menu system.
 while True:
+    print("\n")
     print("--- TASK MANAGER ---")
     user_choice = input("A.Add a new task\nB.Remove a task\nC.Edit a task\nD.View Tasks\nE.Quit\n>")
     user_choice = user_choice.upper()
@@ -129,21 +134,26 @@ while True:
             manager.add_new(user_choice2, user_choice3, user_choice4, user_choice5)
             manager.write_to_csv("task_table.csv")
 
+    # User choice system to remove.
     elif user_choice == "B":
         user_choice6 = input("Please enter which task would you like to remove.\n>")
         manager.remove_task(user_choice6)
-
+    
+    # User choice system to edit tasks.
     elif user_choice == "C":
         user_choice7 = input("Please enter which task would you like to edit.\n>")
         manager.edit_task(user_choice7)
 
+    # User choice system to view tasks.
     elif user_choice == "D":
         manager.view_tasks()
 
+    # Quitting the loop also means quitting program as there is nothing else to run.
     elif user_choice == "E":
         print("Quitting the program.")
         break
 
+    # If anything goes wrong, you are returning to start of the loop again.
     else:
         print('Invalid value. Returning to main menu')
         print("\n")
